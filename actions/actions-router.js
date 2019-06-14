953f20defe668f7530c6b7befd575b135674c598
shanreed <srtech5set@gmail.com>
2019-06-14 09:14:35 -0700

    added routers, server and answered self study questions

---
+++ actions/actions-router.js
@@ -0,0 +1,71 @@
+const db = require('./actions-model');
+
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  // get the actions from the database
+  db.find()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // retrieve a action by id
+  db.findById(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(err => {
+      res.status(200).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // add a action to the database
+  db.add(req.body)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+  if (!name) {
+    res.status(422).json({ message: 'name field required' });
+  }
+  // update actions
+  db.update(id, { name })
+    .then(action => {
+      if (action) {
+        res.json(action);
+      } else {
+        res.status(404).json({ message: 'action not found' });
+      }
+    })
+    .catch(err => {
+      res.status(err).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  // remove actions (inactivate the action)
+  db.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end(); // we could also respond with 200 and a message
+      }
+    })
+    .catch(err => {
+      res.status(404).json({ message: 'action not found' });
+    });
+});
+
+module.exports = router;
---
+++ projects/projects-router.js
@@ -0,0 +1,71 @@
+const db = require('./projects-model');
+
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  // get the projects from the database
+  db.find()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // retrieve a project by id
+  db.findById(req.params.id)
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(200).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // add a project to the database
+  db.add(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+  if (!name) {
+    res.status(422).json({ message: 'name field required' });
+  }
+  // update projects
+  db.update(id, { name })
+    .then(project => {
+      if (project) {
+        res.json(project);
+      } else {
+        res.status(404).json({ message: 'project not found' });
+      }
+    })
+    .catch(err => {
+      res.status(err).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  // remove projects (inactivate the project)
+  db.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end(); // we could also respond with 200 and a message
+      }
+    })
+    .catch(err => {
+      res.status(404).json({ message: 'project not found' });
+    });
+});
+
+module.exports = router;
